b57aa446f4bffade4b781ef944f1f86f3cb7e030
guriqbalsingh727 <guriqbalsingh727@yahoo.com>
2023-01-31 23:26:04 +0530

    Updates

---
+++ precache-manifest.c16c5b40174a834187f2c3fe75a1eb9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "25dcb5ea169d5e331547",
+    "url": "/textutils/static/css/main.3d9e4e41.chunk.css"
+  },
+  {
+    "revision": "25dcb5ea169d5e331547",
+    "url": "/textutils/static/js/main.25dcb5ea.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/textutils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/textutils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "91449b700f0ab8a2a413",
+    "url": "/textutils/static/js/runtime~main.91449b70.js"
+  },
+  {
+    "revision": "d0eca3684b1a2e741eae2db7c8392590",
+    "url": "/textutils/index.html"
+  }
+];
